ce1b7fdea697d03ecdfbac6c18ccbe5c0e7b0399
sharonren02 <91091242+sharonren02@users.noreply.github.com>
2022-11-01 17:17:37 -0400

    Updated test_routes.py
    Creating a separate branch for testing

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -17,27 +17,5 @@ def test_base_route():
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
  
-#tests if content received is in json format
-def test_base_content(): 
-    app = Flash(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/'
-    
-    response = client.get('/')
-    assert response.content_type == "application/json"
 
-def test_predict_route():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/predict'
-    jsonInput = {"school": "", "sex": "", "age": 0, "address": "", "famsize": "", 
-                "pstatus": "", "medu": 0, "fedu": 0, "mjob": "", "fjob": "", 
-                "reason": "", "guardian": "", "traveltime": 0, "studytime": 0, 
-                "failures": 0, "schoolsup": "", "famsup": "", "paid": "", 
-                "activities": "", "nursery": "", "higher": "", "internet": "",
-                "romantic": "", "famrel": 0, "freetime": 0, "goout": 0, 
-                "dalc": 0, "walc": 0, "health": 0, "absences": 0}
-    response = client.post(url, json=jsonInput)
-    assert response.status_code == 200
+
