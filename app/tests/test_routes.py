6000830b08dcd6bfa97162004333cde57effb22f
sharonren02 <91091242+sharonren02@users.noreply.github.com>
2022-11-01 17:22:10 -0400

    Updated test_routes.py with edge case

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -86,3 +86,20 @@ def test_predict_bad_student_route():
     response = client.get('/predict')
     assert response.status_code == 200 
     assert json.loads(response.data) == expectedResponse
+    
+#edge case, with no responses
+def test_predict_edge_case(): 
+        jsonInput =  {"school": "", "sex": "", "age": 0, "address": "", "famsize": "", 
+                "pstatus": "", "medu": 0, "fedu": 0, "mjob": "", "fjob": "", 
+                "reason": "", "guardian": "", "traveltime": 0, "studytime": 0, 
+                "failures": 0, "schoolsup": "", "famsup": "", "paid": "", 
+                "activities": "", "nursery": "", "higher": "", "internet": "",
+                "romantic": "", "famrel": 0, "freetime": 0, "goout": 0, 
+                "dalc":0, "walc": 0, "health": 0, "absences":0}
+            expectedResponse = {
+        "student G3": 0, 
+        }
+    
+    response = client.get('/predict')
+    assert response.status_code == 200 
+    assert json.loads(response.data) == expectedResponse
