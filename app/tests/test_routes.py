878783663ae0d0e245c62cf6fa7acb7e0b2afbac
Timothy Hyun <Timothy>
2022-11-01 18:47:11 -0400

    edited user testing to fit new attributes

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -21,7 +21,7 @@ def test_base_route():
 
 #tests if content received is in json format
 def test_base_content(): 
-    app = Flash(__name__)
+    app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
     url = '/'
@@ -35,70 +35,47 @@ def test_predict_route():
     configure_routes(app)
     client = app.test_client()
     url = '/predict'
-    jsonInput = {"school": "", "sex": "", "age": 0, "address": "", "famsize": "", 
+    jsonInput = {"age": 0, "address": "",
                 "pstatus": "", "medu": 0, "fedu": 0, "mjob": "", "fjob": "", 
-                "reason": "", "guardian": "", "traveltime": 0, "studytime": 0, 
+                "reason": "", "traveltime": 0, "studytime": 0, 
                 "failures": 0, "schoolsup": "", "famsup": "", "paid": "", 
                 "activities": "", "nursery": "", "higher": "", "internet": "",
                 "romantic": "", "famrel": 0, "freetime": 0, "goout": 0, 
                 "dalc": 0, "walc": 0, "health": 0, "absences": 0}
     response = client.post(url, json=jsonInput)
     assert response.status_code == 200
-    
-def test_predict_great_student_route(): 
-    jsonInput =  {"school": "", "sex": "", "age": 22, "address": "U", "famsize": "", 
-                "pstatus": "T", "medu": 0, "fedu": 0, "mjob": "4", "fjob": "4", 
-                "reason": "other", "guardian": "mother", "traveltime": 0, "studytime": 4, 
-                "failures": 1, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
-                "activities": "yes", "nursery": "yes", "higher": "yes", "internet": "yes",
-                "romantic": "no", "famrel": 5, "freetime": 5, "goout": 5, 
-                "dalc": 1, "walc": 1, "health": 5, "absences": 1}
-    expectedResponse = {
-        "student G3": 18, 
-        "probabilities": {
-            15: 0.1
-            20: 0.4, 
-            18: 0.5
-        }
-    }
-    
-    response = client.get('/predict')
-    assert response.status_code == 200 
-    assert json.loads(response.data) == expectedResponse
-    
+        
 def test_predict_bad_student_route(): 
-    jsonInput =  {"school": "", "sex": "", "age": 22, "address": "R", "famsize": "5", 
-                "pstatus": "A", "medu": 0, "fedu": 0, "mjob": "1", "fjob": "1", 
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict'
+    jsonInput =  {"pstatus": "A", "medu": 0, "fedu": 0, "mjob": "1", "fjob": "1", 
                 "reason": "other", "guardian": "mother", "traveltime": 0, "studytime": 2, 
                 "failures": 4, "schoolsup": "no", "famsup": "no", "paid": "no", 
                 "activities": "yes", "nursery": "no", "higher": "no", "internet": "yes",
                 "romantic": "no", "famrel": 3, "freetime": 5, "goout": 5, 
                 "dalc": 3, "walc": 5, "health": 3, "absences": 4}
-    expectedResponse = {
-        "student G3": 7, 
-        "probabilities": {
-            10: 0.1
-            12: 0.4, 
-            7: 0.5
-        }
-    }
     
-    response = client.get('/predict')
-    assert response.status_code == 200 
-    assert json.loads(response.data) == expectedResponse
+    response = client.post('/predict', json=jsonInput)
+    assert response.status_code != 200 
     
 #edge case, with no responses
 def test_predict_edge_case(): 
-        jsonInput =  {"school": "", "sex": "", "age": 0, "address": "", "famsize": "", 
-                "pstatus": "", "medu": 0, "fedu": 0, "mjob": "", "fjob": "", 
-                "reason": "", "guardian": "", "traveltime": 0, "studytime": 0, 
-                "failures": 0, "schoolsup": "", "famsup": "", "paid": "", 
-                "activities": "", "nursery": "", "higher": "", "internet": "",
-                "romantic": "", "famrel": 0, "freetime": 0, "goout": 0, 
-                "dalc":0, "walc": 0, "health": 0, "absences":0}
-            expectedResponse = {
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict'
+    jsonInput =  {"age": 0, "address": "", "famsize": "", 
+            "pstatus": "", "medu": 0, "fedu": 0, "mjob": "", "fjob": "", 
+            "reason": "", "traveltime": 0, "studytime": 0, 
+            "failures": 0, "schoolsup": "", "famsup": "", "paid": "", 
+            "activities": "", "nursery": "", "higher": "", "internet": "",
+            "romantic": "", "famrel": 0, "freetime": 0, "goout": 0, 
+            "dalc":0, "walc": 0, "health": 0, "absences":0}
+    expectedResponse = {
         "student G3": 0, 
-        }
+    }
     
     response = client.get('/predict')
     assert response.status_code == 200 
