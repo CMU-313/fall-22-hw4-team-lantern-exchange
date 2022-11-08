0b88d984d5bcc119354a9a38117244f58325f384
JitongZ <justin_zhoujitong@163.com>
2022-11-07 21:08:00 -0500

    update routes.py to support new features

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -15,56 +15,38 @@ def configure_routes(app):
     def hello():
         return "try the predict route it is great!"
 
-
-    # @app.route('/predict')
-    # def predict():
-    #     #use entries from the query string here but could also use json
-    #     age = request.args.get('age')
-    #     absences = request.args.get('absences')
-    #     health = request.args.get('health')
-    #     data = [[age], [health], [absences]]
-    #     query_df = pd.DataFrame({
-    #         'age': pd.Series(age),
-    #         'health': pd.Series(health),
-    #         'absences': pd.Series(absences)
-    #     })
-    #     query = pd.get_dummies(query_df)
-    #     prediction = clf.predict(query)
-    #     return jsonify(np.ndarray(prediction).item())
     @app.route('/predict', methods=['POST'])
     def predict():
+        """
+        Below is a example POST request using curl:
+            curl -X 'POST' '127.0.0.1:5000/predict' -H 'accept: application/json' \
+                -H 'Content-Type: application/json' -d \
+                    '{"school": "GP",
+                      "traveltime": 4,
+                      "studytime": 4,
+                      "failures": 4,
+                      "paid": "yes",
+                      "activities": "yes",
+                      "nursery": "yes",
+                      "higher": "yes",
+                      "internet": "yes",
+                      "freetime": 5,
+                      "goout": 5,
+                      "Dalc": 5,
+                      "Walc": 5,
+                      "health": 5,
+                      "absences": 93}'
+        """
         if request.method == 'POST':
             json_data = request.get_json()
             df = pd.json_normalize(json_data)
             yesno_cols = ['paid','activities','nursery','higher','internet']
-            df_num = df_num[df_num.columns.difference(yesno_cols)]
+            school_col = ['school']
+            df_num = df[df.columns.difference(yesno_cols)]
             df_yesno = pd.get_dummies(df[yesno_cols].astype(pd.CategoricalDtype(categories=['yes','no'])))
-            df_processed = df_num.join(df_yesno)
-            column_list = list(df_processed.columns) # change order
+            df_school = pd.get_dummies(df[school_col].astype(pd.CategoricalDtype(categories=['GP','MS'])))
+            df_processed = df_num.join(df_yesno).join(df_school)
+            column_list = ['Dalc', 'Walc', 'absences', 'failures', 'freetime', 'goout', 'health', 'studytime', 'traveltime', 'activities_no', 'activities_yes', 'higher_no', 'higher_yes', 'internet_no', 'internet_yes', 'nursery_no', 'nursery_yes', 'paid_no', 'paid_yes', 'school_GP', 'school_MS']
             df_processed = df_processed[column_list]
-            result = model.predict(df_processed)[0]
+            result = int(model.predict(df_processed)[0])
             return jsonify({'prediction': result})
-
-
-"""
-curl -X 'POST' \
-  '127.0.0.1:5000/predict' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "traveltime": 4,
-  "studytime": 4,
-  "failures": 4,
-  "paid": "yes",
-  "activities": "yes",
-  "nursery": "yes",
-  "higher": "yes",
-  "internet": "yes",
-  "freetime": 5,
-  "goout": 5,
-  "dalc": 5,
-  "walc": 5,
-  "health": 5,
-  "absences": 93
-}'
-"""
